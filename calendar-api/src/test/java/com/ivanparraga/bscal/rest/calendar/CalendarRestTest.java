21fcbf96d46dccf56c60fdc33606517311e7c6a5
Iván Párraga García <ivan.parraga.garcia@gmail.com>
2014-02-11 18:48:08 +0100

    tyring to use JerseyTest

--- calendar-api/src/test/java/com/ivanparraga/bscal/rest/calendar/CalendarRestTest.java
+++ calendar-api/src/test/java/com/ivanparraga/bscal/rest/calendar/CalendarRestTest.java
@@ -1,32 +1,40 @@
 package com.ivanparraga.bscal.rest.calendar;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.fail;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.ws.rs.NotFoundException;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.json.JSONException;
+import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 import com.ivanparraga.bscal.core.NoSuchObjectException;
 import com.ivanparraga.bscal.core.calendar.BasicCalendar;
 import com.ivanparraga.bscal.core.calendar.CalendarLao;
 import com.ivanparraga.bscal.core.domain.Calendar;
 
-public class CalendarRestTest {
+public class CalendarRestTest extends JerseyTest {
 	private CalendarLao lao;
 	private CalendarRest rest;
 
-	@BeforeMethod
+	@Override
+	protected Application configure() {
+		return new ResourceConfig(CalendarRest.class);
+	}
+
+//	@Before
 	public void init() {
 		lao = mock(CalendarLao.class);
 		rest = new CalendarRest(lao);
@@ -44,7 +52,7 @@ public class CalendarRestTest {
 		JSONAssert.assertEquals(expected, actualCalendar, false);
 	}
 
-	@Test(expectedExceptions = NotFoundException.class)
+	@Test(expected = NotFoundException.class)
 	public void readNotExisting() throws JSONException {
 		String id = "foo";
 		when(lao.read(id)).thenThrow(new NoSuchObjectException(""));
@@ -92,20 +100,34 @@ public class CalendarRestTest {
 		JSONAssert.assertEquals(expected, actualCalendar, false);
 	}
 
+//	@Test
+//	public void createNullName() throws JSONException {
+//		int year = 2004;
+//
+//		String calendarToCreate = "{\"year\":" + year + "}";
+//
+//		try {
+//			rest.create(calendarToCreate);
+//			fail();
+//		} catch (WebApplicationException e) {
+//			int expectedStatus = 400;
+//			int actualStatus = e.getResponse().getStatus();
+//			assertEquals(actualStatus, expectedStatus);
+//		}
+//	}
+
 	@Test
-	public void createNullName() throws JSONException {
+	public void createNullName() throws Exception {
 		int year = 2004;
 
 		String calendarToCreate = "{\"year\":" + year + "}";
+		Entity<String> userEntity = Entity.entity(calendarToCreate, MediaType.APPLICATION_JSON);
 
-		try {
-			rest.create(calendarToCreate);
-			fail();
-		} catch (WebApplicationException e) {
-			int expectedStatus = 400;
-			int actualStatus = e.getResponse().getStatus();
-			assertEquals(actualStatus, expectedStatus);
-		}
+//		Response response = target("calendar").request().post(userEntity);
+		Response response = target("calendar").request().get();
+		int expectedStatus = 400;
+		int actualStatus = response.getStatus();
+		assertEquals(actualStatus, expectedStatus);
 	}
 
 	@Test
